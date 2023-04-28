0d495706b14d7747fe49fd0bc925e545668c0581
hugoxacme <hugoxacme@gmail.com>
2023-04-28 16:43:07 -0700

    ADD: Operators

--- operators.py
+++ operators.py
@@ -1,4 +1,14 @@
+adition = 10 + 5
+substaccion = 10 - 5 
+multipicacion = 10 * 5
+
+#comparacion
+age = 10 == 5
+age = 10 != 5
+age = 10 > 5
+
 ##Comparadores logicos 
 age_boy = 10
 age_girl = 14
- 
+
+age_boy > 8 and age_girl > 8 
