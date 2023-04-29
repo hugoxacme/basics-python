c348fc62438aac8a34841c1858e8842798025d15
hugoxacme <hugoxacme@gmail.com>
2023-04-28 18:09:36 -0700

    Add:New-develop-data

--- variables.py
+++ variables.py
@@ -1,6 +1,10 @@
 name = 'Hugo'
+Last_name = "Acosta"
 age = 26
 list_shopping = ('milk', 'Eggs', 'apple')
 
+
 print(f"{name} {age}")
 print(list_shopping)
+
+print (name + " " + last_name + " " + age)
