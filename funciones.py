53b45dd0cc07bf5b4ec9856ef04e132ea3bc0b8f
hugoxacme <hugoxacme@gmail.com>
2023-05-04 18:45:30 -0700

    ADD:funciones04

---
+++ funciones.py
@@ -0,0 +1,13 @@
+def bienvenida(nombre, tec):
+    print(f"Bienvenido {nombre} al curso de {tec}")
+
+bienvenida("Hugo", 'Python')
+
+def suma(a, b):
+    return a + b
+
+total = suma(15,20)
+
+print(total)
+
+
